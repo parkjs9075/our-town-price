8a56de2a4c67a7c6e81aeeec1d5de1fde808f6c8
jisun.park <lovelyjs9075@gmail.com>
2020-10-19 22:51:35 +0900

    first commit

---
+++ our-town-price/src/components/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <b-card class="mt-0" header="아파트 실거래가 조회">
+      <b-row class="header">
+        <b-col md="6">아파트 매매 실거래가</b-col>
+        <b-col md="6">분양권 매매 실거래가</b-col>
+      </b-row>
+    </b-card>
+  </div>
+</template>
+
+<script>
+  export default {
+      name: "Index"
+  }
+</script>
+
+<style scoped>
+
+</style>
