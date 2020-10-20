6591408dfbd02ded9b3cbc10c1c8230f9e220c27
jisun.park <lovelyjs9075@gmail.com>
2020-10-20 21:54:42 +0900

    삽질 - CORS 해결 필요

--- our-town-price/src/main.js
+++ our-town-price/src/main.js
@@ -4,11 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
+import axios from 'axios'
+import cors from 'cors'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue)
-Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+Vue.config.productionTip = false;
+Vue.prototype.$http = axios;
 
 /* eslint-disable no-new */
 new Vue({
@@ -16,4 +19,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
