37d097ec05defba82e2a6933ea24e5d77a60f780
parkjs9075 <47883063+parkjs9075@users.noreply.github.com>
2020-11-26 19:47:29 +0900

    Update ApartSalesRequest.kt

--- web.api/src/main/kotlin/com/ourtown/retrofit/model/ApartSalesRequest.kt
+++ web.api/src/main/kotlin/com/ourtown/retrofit/model/ApartSalesRequest.kt
@@ -1,17 +1,22 @@
 package com.ourtown.retrofit.model
 
 data class ApartSalesRequest(
-        val serviceKey: String,
-        val LAWD_CD: String,
-        val DEAL_YMD : String
+    val page: Int = 1,
+    val size: Int = 20,
+    val code: String,
+    val dealYmd : String
 ) {
+    fun validate() {
+        if (code.isBlank()) {
+            throw Exception("Invalid parameter")
+        }
+
+        if (dealYmd.isBlank()) {
+            throw Exception("Invalid parameter")
+        }
+    }
 
     companion object {
-        fun of(code: String, ymd: String) = ApartSalesRequest(
-            serviceKey = KEY,
-            LAWD_CD = code,
-            DEAL_YMD = ymd
-        )
         const val KEY = "Jrz2abzS5ivDmhg9LuMf5sIgK5YRHDXKJtC2GH/nkAdZgzNCYcDIulEom1Dsvm9Ry9UccXY/xDMuPnbWc5v7OQ=="
     }
 }
