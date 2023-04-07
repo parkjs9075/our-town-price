092abac16daea66d559de3e2f4dfb3419b835ff5
jisun.park <lovelyjs9075@gmail.com>
2023-04-07 23:03:25 +0900

    port change 5080 to 80

--- web.front/vue.config.js
+++ web.front/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: "http://localhost:5080",
+                target: "http://localhost:80",
                 pathRewrite: {
                     '^/api': ''
                 }
